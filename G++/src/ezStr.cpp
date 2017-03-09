787b33c72552e3c3e43ee269823aef4b6a80c738
Ryan Chanlatte <ryan.chanlatte@yahoo.com>
2017-03-09 07:04:55 -0500

    First Commit.
    G++ is shipped and it looks good to go on this end.
    
    Next major commit will be adding MSVC functionality. Stay tuned, friends.

---
+++ G++/src/ezStr.cpp
@@ -0,0 +1,90 @@
+#include "ezStr.hpp"
+
+void ezStr::ToUpper(std::string &inputStr)
+{
+	for (size_t charIndex = 0; charIndex < inputStr.length(); charIndex++)
+	{
+		if (islower(inputStr[charIndex]))
+		{
+			inputStr[charIndex] = toupper(inputStr[charIndex]);
+		}
+	}
+}
+
+void ezStr::ToLower(std::string &inputStr)
+{
+	for (size_t charIndex = 0; charIndex < inputStr.length(); charIndex++)
+	{
+		if (isupper(inputStr[charIndex]))
+		{
+			inputStr[charIndex] = tolower(inputStr[charIndex]);
+		}
+	}
+}
+
+void ezStr::Capitalize(std::string &inputStr)
+{
+	for (size_t charIndex = 0; charIndex < inputStr.length(); charIndex++)
+	{
+		if (charIndex == 0 && islower(inputStr[charIndex]))
+		{
+			inputStr[charIndex] = toupper(inputStr[charIndex]);
+			continue;
+		}
+		else if (inputStr[charIndex-1] == newline && islower(inputStr[charIndex]))
+		{
+			inputStr[charIndex] = toupper(inputStr[charIndex]);
+			continue;
+		}
+		else if (inputStr[charIndex-1] == space && islower(inputStr[charIndex]))
+		{
+			inputStr[charIndex] = toupper(inputStr[charIndex]);
+			continue;
+		}
+		else 
+		{
+			inputStr[charIndex] = tolower(inputStr[charIndex]);
+			continue;
+		}
+	}
+}
+
+void ezStr::ReplaceAll(char charToReplace, char charReplacement, std::string &inputStr)
+{
+	std::replace(inputStr.begin(), inputStr.end(), charToReplace, charReplacement);
+}
+
+void ezStr::RemoveChar(std::string &inputStr, char* removeChar)
+{
+	for (size_t charIndex = 0; charIndex < strlen(removeChar); charIndex++)
+	{
+		inputStr.erase(remove(inputStr.begin(), inputStr.end(), removeChar[charIndex]), inputStr.end());
+	}
+}
+
+bool ezStr::Compare(const std::string &firstString, const std::string &secondString)
+{
+	return firstString == secondString ? true : false;
+}
+
+std::vector <std::string> ezStr::ToVector(const std::string &inputStr, const char &delim)
+{
+	std::vector<std::string> tempVec;
+	std::string tempSubStr;
+
+	for (size_t charIndex = 0; charIndex < inputStr.length(); charIndex++)
+	{
+		if (inputStr[charIndex] != delim)
+		{
+			tempSubStr += inputStr[charIndex];
+		}
+		else if (inputStr[charIndex] == delim)
+		{
+			tempVec.push_back(tempSubStr);
+			tempSubStr.clear();
+		}
+	}
+
+	tempVec.push_back(tempSubStr);
+	return tempVec;
+}
---
+++ README.txt
@@ -0,0 +1,5 @@
+This is just a micro (if even that) library with a handful of functions that I find myself frequently creating whenever I make even trivial C++ functions. At the behest of a senior developer, I was informed to compile my own library of functions I find myself reiterating often. 
+
+So, without further ado, here is a small ASCII-based library that makes wrappers for usually verbose functions or tedious, but useful, operations. I will continue to add additional functionality to this library if I deem it necessary (if I forget something or I encounter a bug). Until then, I hope everyone enjoys the library, whomever may stumble upon it.
+
+Thanks for reading.
